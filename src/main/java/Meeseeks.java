26108bef256a584bc1f321558a16410dbf12df6a
xiaotong0329 <xiaotong.huang361042066@gmail.com>
2025-08-20 22:47:42 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Meeseeks.java
@@ -0,0 +1,7 @@
+public class Meeseeks {
+    public static void main(String[] args) {
+
+        System.out.println("Hello! I'm Meeseeks\n" +
+                "What can I do for you?\n" + "Bye. Hope to see you again soon!");
+    }
+}
