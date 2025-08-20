361f1ee72e96cfcb2a3414ad1b41abfd6f4d03c9
xiaotong0329 <xiaotong.huang361042066@gmail.com>
2025-08-20 23:22:44 +0800

    Echo

--- src/main/java/Meeseeks.java
+++ src/main/java/Meeseeks.java
@@ -2,6 +2,17 @@ public class Meeseeks {
     public static void main(String[] args) {
 
         System.out.println("Hello! I'm Meeseeks\n" +
-                "What can I do for you?\n" + "Bye. Hope to see you again soon!");
+                "What can I do for you?\n");
+        Echo("echos");
+
+    }
+
+    public static void Echo(String input) {
+        if (input.equals("bye")) {
+
+            System.out.println("bye\n" + "Bye. Hope to see you again soon!");
+        } else {
+            System.out.println(input + "\n" + "  " + input);
+        }
     }
 }
