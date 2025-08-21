4344e962c33b0b5f06a872402f7f5a6011d1ddd7
xiaotong0329 <xiaotong.huang361042066@gmail.com>
2025-08-21 12:22:22 +0800

    ToDos, Events, Deadlines

--- src/main/java/Meeseeks.java
+++ src/main/java/Meeseeks.java
@@ -1,21 +1,43 @@
 import java.util.ArrayList;
 
 public class Meeseeks {
-    public static ArrayList<String> list = new ArrayList<>();
+    public static ArrayList<Task> list = new ArrayList<>();
 
     public static void main(String[] args) {
 
 
         System.out.println("Hello! I'm Meeseeks\n" +
                 "What can I do for you?\n");
-//        Echo("echos");
-        Add("reading books");
-        Add("playing games");
+        AddTodo("borrow book");
+        AddDeadline("return book", "Sunday");
+        AddEvent("project meeting", "Mon 2pm", "4pm");
         Listing("list");
+
         Marking("mark 2");
         Listing("list");
 
+        Marking("unmark 2");
+        Listing("list");
 
+
+    }
+
+    public static void AddTodo(String description) {
+        Task t = new ToDo(description);
+        list.add(t);
+        printAddedTask(t);
+    }
+
+    public static void AddDeadline(String description, String by) {
+        Task t = new Deadline(description, by);
+        list.add(t);
+        printAddedTask(t);
+    }
+
+    public static void AddEvent(String description, String from, String to) {
+        Task t = new Event(description, from, to);
+        list.add(t);
+        printAddedTask(t);
     }
 
     public static void Echo(String input) {
@@ -27,20 +49,19 @@ public class Meeseeks {
         }
     }
 
-    public static void Add(String input) {
-        String task = "[ ]" + input;
-        list.add(task);
-        System.out.println(input + "\n" + "  " +"added: " + input);
+    private static void printAddedTask(Task t) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + t);
+        System.out.println("Now you have " + list.size() + " tasks in the list.");
     }
 
     public static void Listing(String input) {
-        if(input.equals("list")) {
-            System.out.println("list\n");
-            for (int i = 0; i < list.size(); i++) {
-                int j = i + 1;
-                String s = list.get(i);
+        System.out.println("list\n");
 
-                System.out.println( + j + ": " + s);
+        if(input.equals("list")) {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println((i + 1) + "." + list.get(i));
             }
         }
 
@@ -49,25 +70,18 @@ public class Meeseeks {
     public static void Marking(String input) {
         int index = input.indexOf(" ");
         String s = input.substring(0, index);
+        int num = Integer.parseInt(input.substring(index + 1));
+        Task t = list.get(num - 1);
         if(s.equals("mark")) {
-            int num = Integer.parseInt(input.substring(index + 1));
-            String oritask = list.get(num - 1);
-            int index2 = oritask.indexOf("]");
-            String intertask = oritask.substring(index2 + 1);
-            String task = "[X]" + intertask;
-
-            list.set(num - 1, task);
-            System.out.println( "Nice! I've marked this task as done: \n" + task);
+            t.markAsDone();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("  " + t);
 
         }
         else if(s.equals("unmark")) {
-            int num = Integer.parseInt(input.substring(index + 1));
-            String oritask = list.get(num - 1);
-            int index2 = oritask.indexOf("]");
-            String intertask = oritask.substring(index2 + 1);
-            String task = "[ ]" + intertask;
-
-            System.out.println( "OK, I've marked this task as not done yet: \n" + task);
+            t.markAsNotDone();
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println("  " + t);
         }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    protected String name;
+    protected Boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public String getStatus() {
+        return isDone? "[X]": "[ ]";
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return getStatus() + " " + name;
+    }
+
+}
+
+
