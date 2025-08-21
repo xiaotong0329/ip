158cf2b3d90c4ac67006e002740f27d7d05f60e0
xiaotong0329 <xiaotong.huang361042066@gmail.com>
2025-08-21 12:02:05 +0800

    marking

--- src/main/java/Meeseeks.java
+++ src/main/java/Meeseeks.java
@@ -11,7 +11,9 @@ public class Meeseeks {
 //        Echo("echos");
         Add("reading books");
         Add("playing games");
-        listing("list");
+        Listing("list");
+        Marking("mark 2");
+        Listing("list");
 
 
     }
@@ -26,20 +28,46 @@ public class Meeseeks {
     }
 
     public static void Add(String input) {
-        list.add(input);
+        String task = "[ ]" + input;
+        list.add(task);
         System.out.println(input + "\n" + "  " +"added: " + input);
     }
 
-    public static void listing(String input) {
+    public static void Listing(String input) {
         if(input.equals("list")) {
             System.out.println("list\n");
             for (int i = 0; i < list.size(); i++) {
                 int j = i + 1;
                 String s = list.get(i);
 
-                System.out.println(j + ": " + s);
+                System.out.println( + j + ": " + s);
             }
         }
 
+    }
+
+    public static void Marking(String input) {
+        int index = input.indexOf(" ");
+        String s = input.substring(0, index);
+        if(s.equals("mark")) {
+            int num = Integer.parseInt(input.substring(index + 1));
+            String oritask = list.get(num - 1);
+            int index2 = oritask.indexOf("]");
+            String intertask = oritask.substring(index2 + 1);
+            String task = "[X]" + intertask;
+
+            list.set(num - 1, task);
+            System.out.println( "Nice! I've marked this task as done: \n" + task);
+
+        }
+        else if(s.equals("unmark")) {
+            int num = Integer.parseInt(input.substring(index + 1));
+            String oritask = list.get(num - 1);
+            int index2 = oritask.indexOf("]");
+            String intertask = oritask.substring(index2 + 1);
+            String task = "[ ]" + intertask;
+
+            System.out.println( "OK, I've marked this task as not done yet: \n" + task);
+        }
     }
 }
